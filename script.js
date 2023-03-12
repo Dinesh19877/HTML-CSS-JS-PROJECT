733a9ddff9b06e914b82e809a215512ffe58aa05
doctor1122233 <dineshsharama7799@gmail.com>
2023-03-12 22:21:34 +0545

    DNX TEAM

---
+++ script.js
@@ -0,0 +1,58 @@
+let hamMenuIcon = document.getElementById("ham-menu");
+let navBar = document.getElementById("nav-bar");
+let navLinks = navBar.querySelectorAll("li");
+
+hamMenuIcon.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+  hamMenuIcon.classList.toggle("fa-times");
+});
+navLinks.forEach((navLinks) => {
+  navLinks.addEventListener("click", () => {
+    navBar.classList.remove("active");
+    hamMenuIcon.classList.toggle("fa-times");
+  });
+});
+
+
+var counter = 1;
+setInterval(function () {
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
+  if (counter > 4) {
+    counter = 1;
+  }
+}, 5000);
+
+
+
+let preveiwContainer = document.querySelector('.products-preview');
+let previewBox = preveiwContainer.querySelectorAll('.preview');
+
+document.querySelectorAll('.productitem .itembox1').forEach(itembox1 => {
+  itembox1.onclick = () => {
+    preveiwContainer.style.display = 'flex';
+    let name = itembox1.getAttribute('data-name');
+    previewBox.forEach(preview => {
+      let target = preview.getAttribute('data-target');
+      if (name == target) {
+        preview.classList.add('active');
+      }
+    });
+  };
+});
+
+previewBox.forEach(close => {
+  close.querySelector('.fa-times').onclick = () => {
+    close.classList.remove('active');
+    preveiwContainer.style.display = 'none';
+  };
+});
+
+
+let dineh = document.getElementById("loading");
+function myfuction() {
+  dineh.style.display = "none";
+
+
+
+}
